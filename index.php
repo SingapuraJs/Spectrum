d861a46f20a5cbcade8196fd422a7d0fb42021f8
SingapuraJs <gabrielsammygs@gmail.com>
2023-10-06 00:11:31 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WebSiteOliver</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div id="head">
+        <button class="botao">INFO</button>
+        <button class="botao" id="botao1">LOGIN</button>
+        <button class="botao" id="botao2">CADASTRO</button>
+    </div>
+    <div id="image1"></div>
+    <div id="image2"></div>
+</body>
+</html>
