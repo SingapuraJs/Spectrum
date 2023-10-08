7e4aac55efd717d9b11f12e71af2cd89668294fc
fwrw <heretostayost@gmail.com>
2023-10-07 22:06:36 -0300

    acesso as paginas + organização dos arquivos

---
+++ head.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styles.css">
+</head>
+<body>
+    <header>
+            <div id="menu">
+                
+                    <a href="?pagina=info" id="info">INFO</a>
+
+                <div id="CadLogin">
+
+                    <a href="?pagina=login" id="login">LOGIN</a>
+                    <a href="?pagina=cadastro" id="Registro">REGISTRO</a>  
+
+                </div>
+            </div>
+    </header>
+</body>
+</html> 
--- index.php
+++ index.php
@@ -1,23 +1,50 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WebSiteOliver</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-<body>
-    <header>
-        <div id="container">
-            <div id="menu">
-                    <a href="#" id="info">INFO</a>
-                <div id="container2">
-                    <a href="#" id="login">LOGIN</a>
-                    <a href="#" id="Registro">REGISTRO</a>  
-                </div>
-            </div>
-        </div>
-    </header>
+<?php 
 
-</body>
-</html> 
+// --------------------------------------------------------------------------
+include 'database.php'; // base de dados
+// --------------------------------------------------------------------------
+include 'head.php'; // cabeçalho da página
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+
+
+/*----------ESTRUTURA PARA MUDAR A PÁGINA----------*/ 
+
+if(isset($_GET['pagina'])){
+
+    $pagina = $_GET['pagina'];
+
+}else{
+
+    $pagina = 'home';
+
+};
+
+
+if ($pagina == 'info'){
+
+     include './paginas/info.php'; // página de informações
+
+} elseif ($pagina == 'login'){
+
+     include './paginas/login.php'; // página de login
+
+} elseif ($pagina == 'cadastro'){
+
+     include './paginas/cadastro.php'; // página de cadastro
+
+} else{
+     include './paginas/home.php'; // página home(inicial)
+
+};
+
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+// --------------------------------------------------------------------------
+
+include 'footer.php'; //rodapé da página
+
+
+
+
