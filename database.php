3a5de9514a4d5dcedea468e334e04e4be3efe8f5
SingapuraJs <gabrielsammygs@gmail.com>
2023-10-16 15:04:32 -0300

    Adicionando script do bd

--- database.php
+++ database.php
@@ -10,6 +10,18 @@ $db = "site_db";
 try {
     $pdo = new PDO("mysql:host=$server;dbname=$db", $user, $pass);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $sql = "CREATE TABLE IF NOT EXISTS usuarios (
+        id_usuarios INT NOT NULL AUTO_INCREMENT,
+        usr_usuario VARCHAR(255) NOT NULL,
+        usr_email VARCHAR(255) NOT NULL,
+        usr_senha VARCHAR(255) NOT NULL,
+        usr_telefone VARCHAR(11) NULL,
+        PRIMARY KEY (id_usuarios)
+    ) ENGINE = InnoDB";
+
+    $pdo->exec($sql);
+    echo "Funcionou carambaaa";
 } catch (PDOException $e) {
     die("Falha: " . $e->getMessage());
 }
