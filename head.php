20f0c4a4fc6457c6ea92014ddf55352c3cdd3fe5
fwrw <heretostayost@gmail.com>
2023-10-21 20:45:19 -0300

    trocando echo por if endif

--- head.php
+++ head.php
@@ -1,43 +1,43 @@
-<?php 
-    if(!isset($_SESSION))
-        session_start();
+<?php
+if (!isset($_SESSION))
+    session_start();
 ?>
 
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/styles.css">
 </head>
+
 <body>
+
     <header>
+
         <div id="menu">
+            
             <div id="infoHome">
                 <a href="?pagina=info" id="info">INFO</a>
                 <a href="./index.php" id="home">Inicio</a>
             </div>
 
+        <img src="./image/L02.png" alt="">
 
-            <img src="./image/L02.png" alt="">
-            
-            
-                
-<!-- <a href="?pagina=info" id="info">INFO</a> -->
-                    
-                <div id="CadLogin">
+        <div id="CadLogin">
 
-                    <?php 
-                        if(!isset($_SESSION['id'])){
-                            echo '<a href="?pagina=login" id="login">LOGIN</a>';
-                            echo '<a href="?pagina=cadastro" id="Registro">REGISTRO</a>';  
-                        } else {
-                            echo '<a href="./paginasProtect/logout.php" id="logout">Logout</a>';
-                            echo '<a href="./paginasProtect/perfil.php" id="perfil">Meu perfil</a>';
-                        }
-                    ?>
+            <?php if (!isset($_SESSION['id'])) : ?>
+                <a href="?pagina=login" id="login">LOGIN</a>
+                <a href="?pagina=cadastro" id="Registro">REGISTRO</a>
 
+            <?php else : ?>
 
-                </div>
-            
+                <a href="./paginasProtect/logout.php" id="logout">Logout</a>
+                <a href="./paginasProtect/perfil.php" id="perfil">Meu perfil</a>
+
+            <?php endif; ?>
+
+        </div>
+
     </header>
