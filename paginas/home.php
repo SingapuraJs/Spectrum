8cc74eb2fb26ce3cc694da6ce0d0a6f4617042e0
SingapuraJs <gabrierlsammygs@gmail.com>
2023-10-29 18:28:51 -0300

    Organização de alguns nomes dos arquivos, alguns estavam em 'pt-br', passei para 'en';

--- paginas/home.php
+++
@@ -1,4 +0,0 @@
-<div id="home" class="content" style="text-align: center; ">
-    <h1>opa eae </h1>
-    <a href="#" rel="noopener noreferrer">pipopipo</a>
-</div> 
---
+++ webSitePage/home.php
@@ -0,0 +1,4 @@
+<div id="home" class="content" style="text-align: center; ">
+    <h1>Tela de Inicio</h1>
+    <a href="#" rel="noopener noreferrer"> Seja bem-vindo ! </a>
+</div> 
