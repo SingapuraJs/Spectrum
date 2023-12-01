853a373e50c024519c0a73fa69179c5c8b86253d
Aluno Lab 03 <aluno.lab03@ifpe.edu>
2023-12-01 15:53:46 -0300

    começando a estização do index

--- Views/Home/home.php
+++ Views/Home/home.php
@@ -1,3 +1,11 @@
+<style>
+    .div_informacoes {
+        width: 30%;
+        background-color: red;
+    }
+</style>
+
+
 <div style="height: 100vh;">
 
     <div class="content bg-primary" style="height: 60%; display: flex; align-items: center;">
@@ -7,7 +15,25 @@
         </div>
     </div>
 
-    <div class="content bg-secondary" style="height: 60%;">
-        <!-- aq vem O INFO--> INFORMACOES
+    <div class="content bg-secondary" style="height: 60%; width: 100%;">
+        <!-- aq vem O INFO--> 
+        <p style="text-align: center;">INFORMACOES</p>
+        <div style=" width: 100%; height: 90%; display: flex; justify-content: space-around;">
+            <div class="div_informacoes">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore saepe necessitatibus corrupti nesciunt omnis quae facilis dicta deserunt voluptates incidunt ullam dignissimos quidem nemo blanditiis, at vitae pariatur molestiae aliquid?
+
+            </div>
+
+            <div class="div_informacoes">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore saepe necessitatibus corrupti nesciunt omnis quae facilis dicta deserunt voluptates incidunt ullam dignissimos quidem nemo blanditiis, at vitae pariatur molestiae aliquid?
+
+            </div>
+
+            <div class="div_informacoes">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Labore saepe necessitatibus corrupti nesciunt omnis quae facilis dicta deserunt voluptates incidunt ullam dignissimos quidem nemo blanditiis, at vitae pariatur molestiae aliquid?
+
+            </div>
+
+        </div>
     </div>
 </div>
