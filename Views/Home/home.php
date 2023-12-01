98478405bb76cb9b4e22c462522e0abc91113467
Danielfilipe123 <dfsa1@discente.ifpe.edu.br>
2023-12-01 14:58:40 -0300

    centralização do card

--- Views/Home/home.php
+++ Views/Home/home.php
@@ -1,10 +1,13 @@
 <div style="height: 100vh;">
 
-    <div class="content bg-primary" style="height: 60%;" >
-        <!-- aq vem o card de boas vindas--> INICIO
+    <div class="content bg-primary" style="height: 60%; display: flex; align-items: center;">
+        <!-- aq vem o card de boas vindas-->
+        <div class="bg-light content" style="height: 80%; width: 45%;">
+
+        </div>
     </div>
 
-    <div class="content bg-secondary" style="height: 60%;" >
+    <div class="content bg-secondary" style="height: 60%;">
         <!-- aq vem O INFO--> INFORMACOES
     </div>
-</div> 
+</div>
