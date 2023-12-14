df4dc8ebf28ca08e864eb8a670a7cba34dab3608
fwrw <heretostayost@gmail.com>
2023-12-14 03:05:30 -0300

    campo de posts pronto

--- src/view/user/profile.blade.php
+++ src/view/user/profile.blade.php
@@ -8,16 +8,16 @@
 
         $foto = $userData['foto'] === null ? 'person.svg' : $userData['foto'];
         $bio = $userData['bio'] === null ? 'nada ainda -_-' : $userData['bio'];
-        echo "<pre>";
-        var_dump($_SESSION);
-        echo "</pre>";
+        // echo "<pre>";
+        // var_dump($_SESSION);
+        // echo "</pre>";
 
     @endphp
 
     <div class="border border-dark bg-light m-1">
 
         <div class="d-flex justify-content-center pt-3" style="height: fit-content">
-            <img src="/WebSiteOliver/archives/users/{{ $foto }}" class="shadow-lg " data-bs-toggle="modal" data-bs-target="#minhaModal"
+            <img src="/WebSiteOliver/archives/users/{{ $foto }}" class="shadow-lg p-3"
                 style="img-fluid height: 250px; width:250px; background-color: white; border-radius: 50%">{{-- foto de perfil  --}}
         </div>
 
@@ -28,10 +28,10 @@
 
 
         <div class="row justify-content-center ">
-            <div class="col-xs-12 col-sm-12 col-md-4">
-                <div class="content mx-auto border border-dark d-flex flex-column align-items-center text-center"
+            <div class="col-xs-12 col-sm-12 col-md-4  ">
+                <div class="content mx-auto border rounded border-dark d-flex flex-column align-items-center text-center"
                     style="min-width: 250px; min-height: 135px; max-width: fit-content;">
-                    <small class="bg-dark w-100 m-0 p-0 text-white">Bio</small>
+                    <small class="bg-dark border rounded  w-100 m-0 p-0 text-white">Bio</small>
 
                     <p>{{ $bio }}</p>
                 </div>
@@ -39,53 +39,60 @@
             </div>
         </div>
 
-        <div class="w-50 mx-auto ">
+
+
+        <div class="mx-auto border border rounded border-dark my-2" style="width: 90%">
+            <div class="border-0 border-dark m-0">
+                <div class="row m-1">
+                    
+
+{{-- AQUI DENTRO VIRÁ UM LOOP, QUE RENDERIZARÁ TODAS AS FOTOS DO USUARIO (SE ELE TIVER FOTOS) --}}
+
+                    <div class="col-sm-12 col-md-4 " style="height: 50vh;">
+                        <img src="/WebSiteOliver/archives/users/{{ $foto }}" class="img-fluid rounded p-1" alt="Imagem 1" style="width: 100%; height: 100%; object-fit: cover;">
+                    </div>
+                    
+                    <div class="col-sm-12 col-md-4 " style="height: 50vh;">
+                        <img src="/WebSiteOliver/archives/users/{{ $foto }}" class="img-fluid rounded p-1" alt="Imagem 1" style="width: 100%; height: 100%; object-fit: cover;">
+                    </div>
+                    
+                    <div class="col-sm-12 col-md-4 " style="height: 50vh;">
+                        <img src="/WebSiteOliver/archives/users/{{ $foto }}" class="img-fluid rounded p-1" alt="Imagem 1" style="width: 100%; height: 100%; object-fit: cover;">
+                    </div>
+                    
+                    <div class="col-sm-12 col-md-4 " style="height: 50vh;">
+                        <img src="/WebSiteOliver/archives/users/{{ $foto }}" class="img-fluid rounded p-1" alt="Imagem 1" style="width: 100%; height: 100%; object-fit: cover;">
+                    </div>
+                    
+                    <div class="col-sm-12 col-md-4 " style="height: 50vh;">
+                        <img src="/WebSiteOliver/archives/users/{{ $foto }}" class="img-fluid rounded p-1" alt="Imagem 1" style="width: 100%; height: 100%; object-fit: cover;">
+                    </div>
+                    
+
+
+                </div>
+            </div>
+        </div>
+        
+        
+        
+
+
+        {{-- <div class="w-50 mx-auto ">
             <div class="border border-dark mt-2 mb-5">
                 <div class="row m-1">
-                    <div class="col-sm-12 col-md-6 col-lg-4  mb-1 border border-black">
-                        <img src="/WebSiteOliver/archives/plus.svg" class="img-fluid m-0 w-100 h-100" alt="Imagem 1">
+                    <div class="col-sm-12 col-md-6 col-lg-4  mb-1 border border-black container">
+                        <img src="/WebSiteOliver/archives/users/{{ $foto }}" class="img-fluid m-0 w-100 h-100" alt="Imagem 1">
                     </div>
                 </div>
             </div>
-        </div>
+        </div> --}}
+    
+    
+    
+    
     </div>
 
-    <!-- Botão Bootstrap -->
-    <button type="button" class="btn btn-dark" data-bs-toggle="modal" data-bs-target="#minhaModal">
-        Editar perfil
-    </button>
 
-    <!-- Modal Bootstrap -->
-    <div class="modal fade" id="minhaModal" tabindex="-1" aria-labelledby="minhaModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="minhaModalLabel">Editar perfil</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-
-                    
-                        <form action="/WebSiteOliver/profile/{{ $userData['nome'] }}/post" method="POST">
-                            <div class="form-group">
-                              <label for="image">img test</label>
-                              <input type="text" class="form-control" id="img" name="img">
-                            </div>
-
-                            <div class="form-group">
-                              <label for="desc">desc</label>
-                              <input type="text" class="form-control" id="desc" name="desc">
-                            </div>
-                            <br>
-                            
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                          </form>
-
-
-
-                </div>
-            </div>
-        </div>
-    </div>
 
 @endsection
